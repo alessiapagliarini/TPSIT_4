101c87c51786cdb4b803a409be520afafa4a005c
alessiapagliarini <alessia.pagliarini@hotmail.it>
2017-03-09 11:27:33 +0100

    Add files via upload

---
+++ Verifica0/src/verifica0/Verifica0.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package verifica0;
+
+import java.util.Scanner;
+/**
+ *
+ * @author studente
+ */
+public class Verifica0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Finestra f;
+        
+        f = new Finestra();
+        f.setVisible(true);
+
+        
+    }
+    
+}
